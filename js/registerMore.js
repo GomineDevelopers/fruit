9d6fd54a83bba8cae3334a4d0cc4fbffa4709cb8
RC816 <caiyitian816@126.com>
2018-04-02 09:45:31 +0800

    update

--- js/registerMore.js
+++ js/registerMore.js
@@ -1,14 +1,37 @@
 var registerMoreModel = function () {
     var self = this;
-    self.education = ['大专','本科','硕士','博士','MBA','EMBA','中专','中技','高中','初中','其他'];
-    self.selectedEducation = ko.observable();
-    self.blood = ['A型','B型','O型','AB型'];
-    self.motionFrequency = ['1','2','3','4','5','6','7'];
+    self.educationList = ['大专', '本科', '硕士', '博士', 'MBA', 'EMBA', '中专', '中技', '高中', '初中', '其他'];
+    self.selectedEducation = ko.observable("");
+    self.blood = ['A型', 'B型', 'O型', 'AB型'];
+    self.motionFrequency = ['1', '2', '3', '4', '5', '6', '7'];
+    self.selectedSex = ko.observable("male");
+    self.selectedShow = ko.observable("");
 
     self.locationList = [];
     self.state = ko.observable();
     self.city = ko.observable();
     self.region = ko.observable();
+
+    self.setSex = function (sex) {
+        if (sex != self.selectedSex()) {
+            self.selectedSex(sex);
+        }
+    }
+
+    self.setEducation = function (education) {
+        if (education != self.selectedEducation()) {
+            self.selectedEducation(education);
+        }
+    }
+
+    self.setShow = function (showItem) {
+        if (showItem != self.selectedShow()) {
+            self.selectedShow(showItem);
+        }
+        else if (showItem != "") {
+            self.selectedShow("")
+        }
+    }
 
 }
 
