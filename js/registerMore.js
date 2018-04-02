d526d07e2e9c8e68c066e18baed4231946a3492b
RC816 <caiyitian816@126.com>
2018-04-02 13:51:04 +0800

    update

--- js/registerMore.js
+++ js/registerMore.js
@@ -2,15 +2,16 @@ var registerMoreModel = function () {
     var self = this;
     self.educationList = ['大专', '本科', '硕士', '博士', 'MBA', 'EMBA', '中专', '中技', '高中', '初中', '其他'];
     self.selectedEducation = ko.observable("");
-    self.blood = ['A型', 'B型', 'O型', 'AB型'];
-    self.motionFrequency = ['1', '2', '3', '4', '5', '6', '7'];
+    self.bloodTypeList = ['A型', 'B型', 'O型', 'AB型'];
+    self.selectedBloodType = ko.observable("");
+    self.exerciseList = ['1', '2', '3', '4', '5', '6', '7'];
+    self.selectedExercise = ko.observable("");
     self.selectedSex = ko.observable("male");
-    self.selectedShow = ko.observable("");
-
     self.locationList = [];
-    self.state = ko.observable();
-    self.city = ko.observable();
-    self.region = ko.observable();
+    self.selectedState = ko.observable("");
+    self.selectedCity = ko.observable("");
+    self.selectedRegion = ko.observable("");
+    self.selectedShow = ko.observable("");
 
     self.setSex = function (sex) {
         if (sex != self.selectedSex()) {
@@ -21,6 +22,39 @@ var registerMoreModel = function () {
     self.setEducation = function (education) {
         if (education != self.selectedEducation()) {
             self.selectedEducation(education);
+        }
+    }
+
+    self.setState = function (state) {
+        if (state != self.selectedState()) {
+            self.selectedState(state);
+            self.selectedCity("");
+            self.selectedRegion("");
+        }
+    }
+
+    self.setCity = function (city) {
+        if (city != self.selectedCity()) {
+            self.selectedCity(city);
+            self.selectedRegion("");
+        }
+    }
+
+    self.setRegion = function (region) {
+        if (region != self.selectedRegion()) {
+            self.selectedRegion(region);
+        }
+    }
+
+    self.setExercise = function (exercise) {
+        if (exercise != self.selectedExercise()) {
+            self.selectedExercise(exercise);
+        }
+    }
+
+    self.setBloodType = function (bloodType) {
+        if (bloodType != self.selectedBloodType()) {
+            self.selectedBloodType(bloodType);
         }
     }
 
